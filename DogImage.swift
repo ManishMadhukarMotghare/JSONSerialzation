ca926e6e988c40af337dc1878ed261ddbaabf78c
ManishMadhukarMotghare <40494282+ManishMadhukarMotghare@users.noreply.github.com>
2019-04-08 23:55:35 +0530

    Create DogImage.swift

---
+++ DogImage.swift
@@ -0,0 +1,14 @@
+//
+//  DogImage.swift
+//  Randog
+//
+//  Created by MANISH MOTGHARE on 08/04/19.
+//  Copyright © 2019 CodersRoost. All rights reserved.
+//
+
+import Foundation
+
+struct DogImage : Codable {
+    let status : String
+    let message : String
+}
